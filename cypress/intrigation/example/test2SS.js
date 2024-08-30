38ea423129c67d8c07daa3144b0134642fc350f4
akshay8888509867 <akshayjon88>
2024-08-30 08:09:31 +0530

    new change

--- cypress/intrigation/example/test2SS.js
+++ cypress/intrigation/example/test2SS.js
@@ -1,22 +1,32 @@
 describe('test',function(){
     beforeEach(function() {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        cy.waitForPageLoad();
+      
+       cy.url().should('include', '/login');
+       cy.get("input[name='username']").type('Admin');
+       cy.get("input[type='password']").type("admin123");
+       cy.get('button[type="submit"]').click();
+       cy.url().should('include','/dashboard/index');
+       //  cy.waitForPageLoad();
       });
    it('my test',function(){
   // cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-   cy.url().should('include', '/login');
+  //cy.url({ timeout: 10000 }).should('include', '/login');
+
+ // cy.url().should('include', '/login');
     // Verification: Check that the title is correct
     cy.title().should('eq', 'OrangeHRM');
-   
-    
     });
     it('my second test',function(){
   //  cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-   cy.get("input[name='username']").type('Admin');
-    cy.get("input[type='password']").type("admin123");
-    cy.get('button[type="submit"]').click();
-
-
+    cy.url().should('include','/dashboard/index');
+   // cy.title().should('eq', 'OrangeHRM');
+  //  cy.title().shuold('eq','dashboard');
+  //  cy.get('#oxd-brand-banner')
+ 
     });
+    it('my dasboard test',function(){
+    cy.get(".oxd-userdropdown").click();
+    
+    })
 });
